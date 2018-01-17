c92166560f6ec1bda2b3c07827379a591addd6ea
hunny <hunny@maansoftwares.com>
2018-01-17 19:08:12 +0530

    update appspack and add change permission file

--- scripts/change_permission.sh
+++ scripts/change_permission.sh
@@ -1,4 +1,8 @@
 #chmod -R 775 /var/www/html/sites/*
 chmod -R 775 /var/www/html/sites/*
-chmod 775 all/ default/
-chmod 775 libraries/ modules/ themes/
+chmod 775 /var/www/html/sites/all/
+chmod 775 /var/www/html/sites/default/
+
+chmod 775 /var/www/html/sites/all/libraries/
+chmod 775 /var/www/html/sites/all/modules/
+chmod 775 /var/www/html/sites/all/themes/
